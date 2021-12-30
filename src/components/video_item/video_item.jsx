2f569c0e8cb9225b6e40a8062ab253c2bfb998ba
tjdls111 <tjdls111@naver.com>
2021-12-30 14:44:46 +0900

    사이즈에 따라서 한 줄에 몇개보여줄지

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -3,7 +3,7 @@ import styles from './video_item.module.css'
 import { Col, Row, Navbar,Nav,NavDropdown,Container, Form,Button,FormControl, Card } from 'react-bootstrap';
 
 const VideoItem = ({video:{snippet}}) => (
-      <Col xs xl="3" xxl="2">
+      <Col xs md="6" xl="4" xxl="3">
         <Card className = {styles.video}>
 
           <Card.Img  variant="top" src={snippet.thumbnails.default.url} />
