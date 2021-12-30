0ade67ade866ef0d94b06dcfe1c8c89812558435
tjdls111 <tjdls111@naver.com>
2021-12-30 16:33:30 +0900

    async

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -10,34 +10,36 @@ class Youtube{
     };
   }
 
-  mostPopular() {
-    return fetch(
-      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
-      this.getrequestOptions
-      )
-    .then(response => response.json())
-    .then(result => result.items)
-    .catch(error => console.log('error', error));
+  async mostPopular() {
+    try {
+      const response = await fetch(
+        `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
+        this.getrequestOptions
+      );
+      const result_1 = await response.json();
+      return result_1.items;
+    } catch (error) {
+      return console.log('error', error);
+    }
   }
 
-  search(searchWord){
+  async search(searchWord){
     const url = `https://www.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_YOUTUBE_API_KEY}&part=snippet&maxResults=25&q=${searchWord}&type=video`
     // console.log(url);
-    return axios({
-      method:'get',
-      url : url,
-    })
-    .then((res)=>{
-      const results = res.data.items.map(item=>({
-        ...item, 
-        id:item.id.videoId
+    try {
+      const res = await axios({
+        method: 'get',
+        url: url,
+      });
+      const results = res.data.items.map(item => ({
+        ...item,
+        id: item.id.videoId
       })
-      )
-      return results
-    })
-    .catch((err)=>{
+      );
+      return results;
+    } catch (err) {
       console.log(err);
-    })
+    }
     }
     
 }
