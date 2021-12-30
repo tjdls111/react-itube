5e5c6d59899eb7769805069032584aaccf340f24
tjdls111 <tjdls111@naver.com>
2021-12-30 14:22:57 +0900

    video design card

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -2,21 +2,19 @@ import React from 'react';
 import styles from './video_item.module.css'
 import { Col, Row, Navbar,Nav,NavDropdown,Container, Form,Button,FormControl, Card } from 'react-bootstrap';
 
-const VideoItem = (props) => (
+const VideoItem = ({video:{snippet}}) => (
       <Col xs xl="3" xxl="2">
-      <Card style={{ width: '18rem' }}>
+        <Card className = {styles.video} style={{ width: '18rem' }}>
 
-        {/* <Card.Img variant="top" src={this.props.video.snippet.thumbnails.default.url} /> */}
-        {/* <img src={this.props.video.snippet.thumbnails.default.url} alt="" /> */}
-        {/* <img variant="top" src={this.props.video.snippet.thumbnails.default.url} /> */}
-        <Card.Body>
-          <Card.Title>{props.video.snippet.title}</Card.Title>
-          <Card.Text>
-            {props.video.snippet.description}
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
+          <Card.Img  variant="top" src={snippet.thumbnails.default.url} />
+          <Card.Body>
+            <Card.Title>{snippet.title}</Card.Title>
+            <strong>{snippet.channelTitle}</strong>
+            <Card.Text className={styles.description}>
+              {snippet.description}
+            </Card.Text>
+          </Card.Body>
+        </Card>
             
       </Col>
   );
